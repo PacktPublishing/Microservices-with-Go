338ef732b987f3a881ce50c2c7fdfd0195c9c9a4
Alexander Shuiskov <ashuiskov@gmail.com>
2023-08-03 14:09:47 -0400

    Fix MovieDetails struct JSON mappings

--- Chapter02/src/movie/pkg/model/movie.go
+++ Chapter02/src/movie/pkg/model/movie.go
@@ -4,6 +4,6 @@ import "movieexample.com/metadata/pkg/model"
 
 // MovieDetails includes movie metadata its aggregated rating.
 type MovieDetails struct {
-	Rating   *float64       `json:"rating,omitEmpty"`
-	Metadata model.Metadata `json:"metadata`
+	Rating   *float64       `json:"rating,omitempty"`
+	Metadata model.Metadata `json:"metadata"`
 }
